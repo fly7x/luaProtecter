b7f72a2f43ec745b759ed4e8df05f0ed22d9c1e8
fly7x <moode6019@gmail.com>
2026-08-22 00:24:12 +0400

    Update compiler.hpp

--- src/compiler.hpp
+++ src/compiler.hpp
@@ -9,13 +9,6 @@ class Compiler
 public:
     Compiler() = default;
 
-    /*
-     * Compile supported Luau source into our custom VM
-     * instruction format.
-     *
-     * This is NOT native Luau bytecode.
-     * The resulting Bytecode is consumed by VM::execute().
-     */
     Bytecode compile(
         const std::string& source
     ) const;
