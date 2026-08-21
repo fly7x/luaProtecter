1efdacc716168870ea21ebea5e6b7110d46a5fb6
fly7x <moode6019@gmail.com>
2026-08-22 00:25:26 +0400

    Update vm.hpp

--- src/vm.hpp
+++ src/vm.hpp
@@ -9,14 +9,6 @@ class VM
 public:
     VM() = default;
 
-    /*
-     * Execute custom LVM bytecode.
-     *
-     * output receives everything printed by the VM.
-     *
-     * Returns true on success.
-     * Returns false on invalid bytecode/runtime failure.
-     */
     bool execute(
         const Bytecode& bytecode,
         std::string& output
