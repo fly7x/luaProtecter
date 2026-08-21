5ee1e26c3d210e6fcae81fb019b92e018e893b09
fly7x <moode6019@gmail.com>
2026-08-22 00:37:27 +0400

    Update transformer.hpp

--- src/transformer.hpp
+++ src/transformer.hpp
@@ -2,11 +2,20 @@
 
 #include "bytecode.hpp"
 
+#include <cstdint>
+#include <string>
+
 class Transformer
 {
 public:
     Transformer() = default;
 
+    /*
+     * Protect compiled Luau bytecode.
+     *
+     * This does NOT change the Luau instruction semantics.
+     * It creates a package containing protected bytecode.
+     */
     Bytecode protect(
         const Bytecode& bytecode
     ) const;
