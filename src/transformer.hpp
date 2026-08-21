3553269e785c06d073bf889a3fb12a051e1ea329
fly7x <moode6019@gmail.com>
2026-08-21 23:49:24 +0400

    Update transformer.hpp

--- src/transformer.hpp
+++ src/transformer.hpp
@@ -7,9 +7,17 @@ class Transformer
 public:
     Transformer() = default;
 
-    // Takes valid Luau source and returns valid,
-    // source-level-obfuscated Luau.
-    std::string transform(
-        const std::string& source
+    /*
+     * Takes REAL Luau bytecode and packages it for
+     * the custom VM/protection layer.
+     *
+     * Input:
+     *     Luau bytecode
+     *
+     * Output:
+     *     Protected custom VM package
+     */
+    std::string protect(
+        const std::string& bytecode
     );
 };
