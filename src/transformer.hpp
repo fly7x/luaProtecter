89dbbc7f91fb18b1291a8d4f7cbef3f367336b34
fly7x <moode6019@gmail.com>
2026-08-22 00:24:47 +0400

    Update transformer.hpp

--- src/transformer.hpp
+++ src/transformer.hpp
@@ -7,16 +7,7 @@ class Transformer
 public:
     Transformer() = default;
 
-    /*
-     * Protect custom VM bytecode.
-     *
-     * Input:
-     *     LVM1 custom bytecode
-     *
-     * Output:
-     *     LPRO protected package
-     */
     Bytecode protect(
         const Bytecode& bytecode
-    );
+    ) const;
 };
