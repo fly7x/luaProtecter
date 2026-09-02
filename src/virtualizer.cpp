2b16066277fd1f8531f914e9d64c747f385adecf
fly7x <moode6019@gmail.com>
2026-09-02 21:27:01 +0400

    Update virtualizer.cpp

--- src/virtualizer.cpp
+++ src/virtualizer.cpp
@@ -10,6 +10,17 @@ Virtualizer::Virtualizer(uint64_t seed) : seed_(seed) {
     if (seed_ == 0) seed_ = 0x9E3779B97F4A7C15ULL;
 }
 
+std::string Virtualizer::bytecodeToLuaTable(const std::vector<uint8_t>& data) const {
+    std::stringstream ss;
+    ss << "{";
+    for (size_t i = 0; i < data.size(); ++i) {
+        if (i > 0) ss << ",";
+        ss << static_cast<int>(data[i]);
+    }
+    ss << "}";
+    return ss.str();
+}
+
 std::string Virtualizer::generateVMName() const {
     uint64_t r = seed_ ^ 0x9e3779b97f4a7c15ULL;
     r ^= r >> 30;
@@ -19,17 +30,6 @@ std::string Virtualizer::generateVMName() const {
     r ^= r >> 31;
     std::stringstream ss;
     ss << "_vm" << std::hex << (r & 0xFFFFFFFF);
-    return ss.str();
-}
-
-std::string Virtualizer::bytecodeToLuaTable(const std::vector<uint8_t>& data) const {
-    std::stringstream ss;
-    ss << "{";
-    for (size_t i = 0; i < data.size(); ++i) {
-        if (i > 0) ss << ",";
-        ss << static_cast<int>(data[i]);
-    }
-    ss << "}";
     return ss.str();
 }
 
@@ -80,22 +80,19 @@ std::string Virtualizer::generateInterpreter(const std::string& bytecodeTableVar
                                              bool antiDebug) const {
     std::stringstream code;
 
-    // ----- VM state -----
     code << "local pc = 1\n";
     code << "local reg = {}\n";
     code << "local stack = {}\n";
     code << "local top = 0\n";
     code << "local bc = " << bytecodeTableVar << "\n";
 
-    // ----- Decrypt bytecode in place -----
     code << "bc = _decrypt(bc, " << seedVar << ")\n";
 
-    // ----- Anti-debug hook -----
     if (antiDebug) {
         code << "_antiDebug()\n";
     }
 
-    // ----- The main loop (flattened for obfuscation) -----
+    // Flattened dispatch loop
     code << "local state = 0\n";
     code << "local dispatch = {\n";
     code << "  [0] = function()\n";
@@ -109,78 +106,76 @@ std::string Virtualizer::generateInterpreter(const std::string& bytecodeTableVar
     code << "      local e = (inst >> 8) & 0xFFFFFF\n";
     code << "      if e & 0x800000 then e = e - 0x1000000 end\n";
 
-    // ----- Opcode dispatch (big if-elseif chain) -----
-    // We'll include handlers for the most common Luau opcodes.
-    // This is a subset – you can expand it.
-    code << "      if op == 0x01 then -- MOVE\n";
-    code << "        reg[a] = reg[b]\n";
-    code << "      elseif op == 0x02 then -- LOADK\n";
-    code << "        local k = bc[pc+1]\n";
-    code << "        reg[a] = k\n";
-    code << "        pc = pc + 1\n";
-    code << "      elseif op == 0x03 then -- LOADNIL\n";
-    code << "        for i = a, b do reg[i] = nil end\n";
-    code << "      elseif op == 0x04 then -- LOADBOOL\n";
-    code << "        reg[a] = (b ~= 0)\n";
-    code << "      elseif op == 0x05 then -- ADD\n";
-    code << "        reg[a] = reg[b] + reg[c]\n";
-    code << "      elseif op == 0x06 then -- SUB\n";
-    code << "        reg[a] = reg[b] - reg[c]\n";
-    code << "      elseif op == 0x07 then -- MUL\n";
-    code << "        reg[a] = reg[b] * reg[c]\n";
-    code << "      elseif op == 0x08 then -- DIV\n";
-    code << "        reg[a] = reg[b] / reg[c]\n";
-    code << "      elseif op == 0x09 then -- MOD\n";
-    code << "        reg[a] = reg[b] % reg[c]\n";
-    code << "      elseif op == 0x0A then -- POW\n";
-    code << "        reg[a] = reg[b] ^ reg[c]\n";
-    code << "      elseif op == 0x0B then -- UNM\n";
-    code << "        reg[a] = -reg[b]\n";
-    code << "      elseif op == 0x0C then -- NOT\n";
-    code << "        reg[a] = not reg[b]\n";
-    code << "      elseif op == 0x0D then -- LEN\n";
-    code << "        reg[a] = #reg[b]\n";
-    code << "      elseif op == 0x0E then -- CONCAT\n";
-    code << "        local s = ''\n";
-    code << "        for i = b, c do s = s .. reg[i] end\n";
-    code << "        reg[a] = s\n";
-    code << "      elseif op == 0x0F then -- JMP\n";
-    code << "        pc = pc + d\n";
-    code << "      elseif op == 0x10 then -- EQ\n";
-    code << "        if (reg[b] == reg[c]) == (a ~= 0) then\n";
-    code << "          pc = pc + d\n";
-    code << "        end\n";
-    code << "      elseif op == 0x11 then -- LT\n";
-    code << "        if (reg[b] < reg[c]) == (a ~= 0) then\n";
-    code << "          pc = pc + d\n";
-    code << "        end\n";
-    code << "      elseif op == 0x12 then -- LE\n";
-    code << "        if (reg[b] <= reg[c]) == (a ~= 0) then\n";
-    code << "          pc = pc + d\n";
-    code << "        end\n";
-    code << "      elseif op == 0x13 then -- CALL\n";
-    code << "        local func = reg[a]\n";
-    code << "        local nargs = b - 1\n";
-    code << "        local nresults = c - 1\n";
-    code << "        local args = {}\n";
-    code << "        for i = 1, nargs do args[i] = reg[a + i] end\n";
-    code << "        local results = { func(table.unpack(args)) }\n";
-    code << "        for i = 1, nresults do reg[a + i - 1] = results[i] end\n";
-    code << "        if nresults == 0 then top = #results end\n";
-    code << "      elseif op == 0x14 then -- RETURN\n";
-    code << "        local n = b - 1\n";
-    code << "        local ret = {}\n";
-    code << "        for i = 1, n do ret[i] = reg[a + i - 1] end\n";
-    code << "        return table.unpack(ret)\n";
-    code << "      else\n";
-    code << "        -- Unknown opcode – skip\n";
-    code << "      end\n";
+    // Opcode handlers (expanded list)
+    code << R"(
+      if op == 0x01 then -- MOVE
+        reg[a] = reg[b]
+      elseif op == 0x02 then -- LOADK
+        local k = bc[pc+1]
+        reg[a] = k
+        pc = pc + 1
+      elseif op == 0x03 then -- LOADNIL
+        for i = a, b do reg[i] = nil end
+      elseif op == 0x04 then -- LOADBOOL
+        reg[a] = (b ~= 0)
+      elseif op == 0x05 then -- ADD
+        reg[a] = reg[b] + reg[c]
+      elseif op == 0x06 then -- SUB
+        reg[a] = reg[b] - reg[c]
+      elseif op == 0x07 then -- MUL
+        reg[a] = reg[b] * reg[c]
+      elseif op == 0x08 then -- DIV
+        reg[a] = reg[b] / reg[c]
+      elseif op == 0x09 then -- MOD
+        reg[a] = reg[b] % reg[c]
+      elseif op == 0x0A then -- POW
+        reg[a] = reg[b] ^ reg[c]
+      elseif op == 0x0B then -- UNM
+        reg[a] = -reg[b]
+      elseif op == 0x0C then -- NOT
+        reg[a] = not reg[b]
+      elseif op == 0x0D then -- LEN
+        reg[a] = #reg[b]
+      elseif op == 0x0E then -- CONCAT
+        local s = ''
+        for i = b, c do s = s .. reg[i] end
+        reg[a] = s
+      elseif op == 0x0F then -- JMP
+        pc = pc + d
+      elseif op == 0x10 then -- EQ
+        if (reg[b] == reg[c]) == (a ~= 0) then
+          pc = pc + d
+        end
+      elseif op == 0x11 then -- LT
+        if (reg[b] < reg[c]) == (a ~= 0) then
+          pc = pc + d
+        end
+      elseif op == 0x12 then -- LE
+        if (reg[b] <= reg[c]) == (a ~= 0) then
+          pc = pc + d
+        end
+      elseif op == 0x13 then -- CALL
+        local func = reg[a]
+        local nargs = b - 1
+        local nresults = c - 1
+        local args = {}
+        for i = 1, nargs do args[i] = reg[a + i] end
+        local results = { func(table.unpack(args)) }
+        for i = 1, nresults do reg[a + i - 1] = results[i] end
+        if nresults == 0 then top = #results end
+      elseif op == 0x14 then -- RETURN
+        local n = b - 1
+        local ret = {}
+        for i = 1, n do ret[i] = reg[a + i - 1] end
+        return table.unpack(ret)
+      else
+        -- Unknown opcode – skip
+      end
+)";
     code << "      pc = pc + 1\n";
     code << "    end\n";
     code << "  end,\n";
     code << "}\n";
-
-    // ----- Execute dispatch[0] -----
     code << "dispatch[0]()\n";
 
     return code.str();
@@ -194,31 +189,22 @@ std::string Virtualizer::emitVirtualizedScript(const Bytecode& obfuscatedBytecod
     }
 
     std::string vmName = generateVMName();
-    std::string bytecodeVar = vmName .. "_bc";
+    std::string bytecodeVar = vmName + "_bc";   // FIXED: use + not ..
     std::string seedVar = std::to_string(seed_);
 
     std::stringstream script;
 
-    // 1. Anti-debug
     if (options.antiDebug) {
         script << generateAntiDebug();
     }
-
-    // 2. Decryptor
     script << generateDecryptor();
-
-    // 3. Bytecode as table
     script << "local " << bytecodeVar << " = " << bytecodeToLuaTable(data) << "\n";
-
-    // 4. The interpreter
     script << generateInterpreter(bytecodeVar, seedVar, options.antiDebug);
 
-    // 5. Wrap in a function to hide globals
     std::stringstream finalScript;
     finalScript << "return function()\n";
     finalScript << script.str();
     finalScript << "end\n";
-
     return finalScript.str();
 }
 
